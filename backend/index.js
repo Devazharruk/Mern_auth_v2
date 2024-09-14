05ba7285272f74de31b0d77ffcdb5fe02a01a595
Devazharruk <azharruk66@gmail.com>
2024-09-14 09:35:35 -0700

    c

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,9 @@ app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
 
 app.use("/api/auth", authRoutes);
-
+app.get("/",(req,res)=>{
+	res.send("hello world")
+})
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
